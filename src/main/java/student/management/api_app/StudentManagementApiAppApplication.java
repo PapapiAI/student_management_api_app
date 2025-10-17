73df755f9d256cae04949a90a869cddce8f497a1
huynh hai thien <huynhhaithien@gmail.com>
2025-10-17 14:15:18 +0700

    Lession 1 - Initial commit - base source for student_management_api_app with API /health

---
+++ src/main/java/student/management/api_app/StudentManagementApiAppApplication.java
@@ -0,0 +1,13 @@
+package student.management.api_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApiAppApplication.class, args);
+	}
+
+}
